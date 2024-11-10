5e1da3e814620936674ffe62c8e235eecfff1801
davis.nguyenduy <davis.nguyenduy@gmail.com>
2024-11-10 23:15:46 +0700

    Refactor

--- apis/src/FlashCard.Api/Contexts/CurrentUser.cs
+++ apis/src/FlashCard.Api/Contexts/CurrentUser.cs
@@ -5,9 +5,7 @@ namespace FlashCard.Api.Contexts;
 public class CurrentUser : ICurrentUser
 {
     public string? UserId { get; internal set; }
-
     public string? Username { get; internal set; }
-
     public bool IsAuthenticated { get; internal set; }
 
 }
